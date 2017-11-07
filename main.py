a76a5b0bc6b6436cc94f031809df61b0d8e6b3b5
arturgmlcosta <33459942+arturgmlcosta@users.noreply.github.com>
2017-11-07 17:46:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 This script creates a full schedule for a team. Accepts staff_list.csv and hours_list.csv as inputs (see below for details) and outputs rota.csv
 '''
 
-import staff_gen
-import hours_gen
+import gen
 
-staff_gen.generate('staff_list.csv')
-hours_gen.generate('hours_list.csv')
+staff_gen.generate('staff_list.csv',staff)
+hours_gen.generate('hours_list.csv',hours)
 
 
 
