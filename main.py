2a1f175458a1696c4ddea67b404452ad88c6a4b6
arturgmlcosta <33459942+arturgmlcosta@users.noreply.github.com>
2017-11-07 18:06:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,22 @@ This script creates a full schedule for a team. Accepts staff_list.csv and hours
 
 import gen
 
-staff_gen.generate('staff_list.csv',staff)
-hours_gen.generate('hours_list.csv',hours)
+staff = staff_gen.generate('staff_list.csv')
+hours = hours_gen.generate('hours_list.csv')
+
+criteria = [
+member['week'][shift['week day']] == '',						#staff member not working that day
+member['max hours'] - member['current hours'] <= shift['end'] - shift['begin'],
+																#enough hours left to do shift
+len([i for i in member['week'] if member['week'][i] != 0]) <5,	#maximum 5 working days per week
+shift['week day'] not in member['days off']
+]
+
+def allocate (staff,hours,criteria):
+  for member in staff:
+    for shift in hours:
+      if all(criteria):
+        #allocate
 
 
-
-
-
-
-
-#from staff_dir import staff
-#from hours_dir import hours
 
