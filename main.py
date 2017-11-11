3b66715ff96f0f83c1cd9b0063b57180369960bd
arturgmlcosta <33459942+arturgmlcosta@users.noreply.github.com>
2017-11-11 22:50:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ staff = staff_gen.generate('staff_list.csv')
 hours = hours_gen.generate('hours_list.csv')
 
 criteria = [
-member['week'][shift['week day']] == '',						#staff member not working that day
-member['max hours'] - member['current hours'] <= shift['end'] - shift['begin'],
-																#enough hours left to do shift
-len([i for i in member['week'] if member['week'][i] != 0]) <5,	#maximum 5 working days per week
-shift['week day'] not in member['days off']
+	member['week'][shift['week day']] == '',		#staff member not working that day
+	member['max hours'] - member['current hours'] <= shift['end'] - shift['begin'],
+													#enough hours left to do shift
+	len([i for i in member['week'] if member['week'][i] != 0]) <5,
+													#maximum 5 working days per week
+	shift['week day'] not in member['days off']
 ]
 
 def allocate (staff,hours,criteria):
